72ce97eb8a2c767d58f88d8dc460cdc144906f5c
[tirael] <nastya.levina@mail.ru>
2021-12-06 23:51:42 +0300

    Home work 5 test junit5 + report

--- src/test/java/org/example/TestSearchForAnonExistentRecipe.java
+++ src/test/java/org/example/TestSearchForAnonExistentRecipe.java
@@ -1,39 +1,39 @@
 package org.example;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class searchForAnonExistentRecipeTest {
-    private WebDriver driver;
+public class TestSearchForAnonExistentRecipe {
 
-    @Before
-    public void setUp() {
+    private static WebDriver driver;
 
+    @BeforeAll
+    public static void setUp() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
 
-    @After
-    public void tearDown() {
+    @AfterAll
+    public static void tearDown() {
 
         driver.quit();
     }
 
     @Test
-    public void searchForAnonExistentRecipe(){
+    public void testSearchForAnonExistentRecipe(){
         driver.get("https://www.povarenok.ru/");
         driver.findElement(By.cssSelector("body > div.page-width > header > div > div > div.search-wrap > form > input[type=text]:nth-child(1)")).sendKeys("Тирлитуредуй");
         driver.findElement(By.cssSelector("body > div.page-width > header > div > div > div.search-wrap > form > input[type=submit]:nth-child(2)")).click();
         String text = driver.findElement(By.xpath("/html/body/div[3]/div[3]/div/div[1]/section[1]/div[4]/div[2]/strong")).getText();
-        Assert.assertEquals(text,"ничего не найдено");
+        Assertions.assertEquals(text,"ничего не найдено");
         driver.close();
     }
 
