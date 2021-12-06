f3e9cdcaaf9406ddbe4db9ffd28ef239226ce121
16724514 <belyakov.i.va@sberbank.ru>
2021-12-06 23:34:15 +0300

    junit5 + report

--- src/test/java/org/example/TestLinkForgotPassword.java
+++ src/test/java/org/example/TestLinkForgotPassword.java
@@ -1,38 +1,37 @@
 package org.example;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class linkForgotPasswordTest {
-    private WebDriver driver;
+public class TestLinkForgotPassword {
 
-    @Before
-    public void setUp() {
+    private static WebDriver driver;
 
+    @BeforeAll
+    public static void setUp() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
 
-    @After
-    public void tearDown() {
-
+    @AfterAll
+    public static void tearDown() {
         driver.quit();
     }
 
     @Test
-    public void linkForgotPassword(){
+    public void testLinkForgotPassword(){
         driver.get("https://www.povarenok.ru/");
         driver.findElement(By.linkText("Вспомнить пароль")).click();
         String text = driver.findElement(By.cssSelector("body > div.main-form > div:nth-child(1) > div > h1")).getText();
-        Assert.assertEquals(text,"Забыли пароль?");
+        Assertions.assertEquals(text,"Забыли пароль?");
         driver.close();
     }
 }
